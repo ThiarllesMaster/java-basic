b6390702f6c6ebf0986c20cd2a2a1831282c0016
Thiarlles Gomes <thiarlles.gomes@feedzai.com>
2022-01-03 23:01:21 +0000

    Third commit Hackerrank

---
+++ estudos-java-basic/src/test/java/hackerrank/Solution.java
@@ -0,0 +1,29 @@
+package hackerrank;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = format.format(payment);
+        format = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        String india = format.format(payment);
+        format = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = format.format(payment);
+        format = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = format.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
